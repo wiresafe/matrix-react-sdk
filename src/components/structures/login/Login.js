55da07bcfff07fca6d89d12b882280385192db48
Dave Duran <dave@yodata.io>
2017-09-16 04:51:34 -0700

    Revert "firebase-auth wip"
    This reverts commit 00c5192

--- src/components/structures/login/Login.js
+++ src/components/structures/login/Login.js
@@ -367,7 +367,7 @@ module.exports = React.createClass({
             let username = user.uid
             let phoneCountry = '1' // should this be 1 or USA?
             let phoneNumber = user.phoneNumber;
-            let password = user.getIdToken();
+            let password = user.stsTokenManager && user.stsTokenManager.accessToken;
             console.log('CALLING PASSWORD LOGIN WITH CREDENTIALS', { username, phoneCountry, phoneNumber, password })
             this.onPasswordLogin(username, phoneCountry, phoneNumber, password)
         }
